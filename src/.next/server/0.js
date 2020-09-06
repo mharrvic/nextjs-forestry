3be691e13fbccb48c4ec29eb8daeb536dba9dae6
mharrvic <mharrvic@gmail.com>
2020-09-06 10:26:21 +0800

    updated gitignore

---
+++ src/.next/server/0.js
@@ -0,0 +1,15 @@
+exports.ids = [0];
+exports.modules = {
+
+/***/ "./data/config.json":
+/*!**************************!*\
+  !*** ./data/config.json ***!
+  \**************************/
+/*! exports provided: title, description, repositoryUrl, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"title\\\":\\\"Brevifolia\\\",\\\"description\\\":\\\"Starter blog using Tina CMS with Next.js.\\\",\\\"repositoryUrl\\\":\\\"https://github.com/kendallstrautman/brevifolia-next-tinacms\\\"}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2RhdGEvY29uZmlnLmpzb24uanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./data/config.json\n");
+
+/***/ })
+
+};;
